f17a0adfe07e6cd303a883874557f9bec597f8ca
DumLiviu <158556659+DumLiviu@users.noreply.github.com>
2024-06-30 17:56:46 +0300

    Finalizare tema

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,24 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const listE = document.querySelector('.gallery');
+const photoM = createGalleryItem(galleryItems);
+
+function createGalleryItem(e) {
+  return e
+    .map(({ preview, original, description }) => {
+      return `<a class='gallery__link' href="${preview}">
+            <img class='gallery__image' src = ${original} alt = ${description} /> 
+            </a>`;
+    })
+    .join('');
+}
+listE.insertAdjacentHTML('beforeend', photoM);
+const galleryH = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+galleryH.on('show.simplelightbox');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const saveFormState = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}, 5000);
+
+const loadFormState = () => {
+  const savedState = localStorage.getItem(STORAGE_KEY);
+  if (savedState) {
+    const { email, message } = JSON.parse(savedState);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+};
+
+form.addEventListener('input', saveFormState);
+form.addEventListener('submit', handleSubmit);
+
+document.addEventListener('DOMContentLoaded', loadFormState);
